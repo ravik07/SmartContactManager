ed81356838514fd0aa70b8755bda1f211c04a730
ravik07 <kharteravi7@gmail.com>
2025-01-07 20:08:50 +0530

    added controller class for frontend, completed basic frontend

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,54 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String home(Model model) {
+        System.out.println("Home page handler");
+        // sending data to view
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtubeChannel", "Learn Code With Durgesh");
+        model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
+        return "home";
+    }
+
+    // about route
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        model.addAttribute("isLogin", true);
+        System.out.println("About page loading");
+        return "about";
+    }
+
+    // services
+
+    @RequestMapping("/services")
+    public String servicesPage() {
+        System.out.println("services page loading");
+        return "services";
+    }
+
+    // contact page
+
+    @GetMapping("/contact")
+    public String contact() {
+        return new String("contact");
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return new String("login");
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+}
---
+++ src/main/java/com/scm/entities/User.java
@@ -0,0 +1,13 @@
+package com.scm.entities;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String userId;
+	
+	private String name;
+	
+	private String email;
+}
